cd6079cbd88298415dc49479783e727abd2404c5
netzzz <netz@email.com>
2015-10-24 18:39:53 +0100

    basic jessey webservice

---
+++ JerseyWebService/jerseywslogic/src/main/java/pravin/practws/App.java
@@ -0,0 +1,13 @@
+package pravin.practws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
